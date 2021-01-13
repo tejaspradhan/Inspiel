a4cf7fc513eba8ad2b5a20b1527fd67ce70d362e
chiragvaswani <chiragvaswani751@gmail.com>
2021-01-13 20:12:57 +0530

    Front end release

--- js/main (2).js
+++ js/main (2).js
@@ -1,15 +1,15 @@
-const loader = document.querySelector('.loader');
-const main = document.querySelector('.main');
-const swipe = document.querySelector('.tagline');
-const body= document.querySelector('body');
+const loader = document.querySelector(".loader");
+const main = document.querySelector(".main");
+const swipe = document.querySelector(".tagline");
+const body = document.querySelector("body");
 function init() {
   setTimeout(() => {
     loader.style.opacity = 0;
-    loader.style.display = 'none';
+    loader.style.display = "none";
     swipe.style.opacity = 0;
-    swipe.style.display = 'none';
-    body.style.backgroundImage = 'none';
-    main.style.display = 'block';
+    swipe.style.display = "none";
+    body.style.backgroundImage = "none";
+    main.style.display = "block";
     setTimeout(() => (main.style.opacity = 1), 50);
   }, 5000);
 }
